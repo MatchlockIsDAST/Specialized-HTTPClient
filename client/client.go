347736a4a5b309754038714d8bfd7ac779dafe74
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-11-05 00:11:59 +0900

    計測や表示判定に関する特殊クライアントを提供

--- client/client.go
+++ client/client.go
@@ -3,6 +3,12 @@ package client
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/MatchlockIsDAST/httpmesconv/stringto"
+	"github.com/MatchlockIsDAST/httpmesconv/tostring"
+
+	"github.com/MatchlockIsDAST/sphttpclient/judgment"
 )
 
 //New Clientを生成します
@@ -15,6 +21,8 @@ type Client interface {
 	ChangeClient(httpclient http.Client)
 	GetClient() http.Client
 	Do(req *http.Request) (resp *http.Response, err error)
+	TimeBaseJudgeDo(elapsedMin, elapsedMax time.Duration, req *http.Request) (resp *http.Response, flag bool, err error)
+	DisplayBaseJudgeDo(included string, req *http.Request) (resp *http.Response, flag bool, err error)
 }
 
 //Client 内部の情報を定義します
@@ -30,6 +38,7 @@ func (c *client) GetClient() http.Client {
 	return c.httpclient
 }
 
+//通常のClientを提供する
 func (c *client) Do(req *http.Request) (resp *http.Response, err error) {
 	resp, err = c.httpclient.Do(req)
 	if err != nil {
@@ -38,4 +47,28 @@ func (c *client) Do(req *http.Request) (resp *http.Response, err error) {
 	}
 	defer resp.Body.Close()
 	return resp, err
+}
+
+//時間計測を同時に行うClientを提供する
+func (c *client) TimeBaseJudgeDo(elapsedMin, elapsedMax time.Duration, req *http.Request) (resp *http.Response, flag bool, err error) {
+	start := time.Now()
+	resp, err = c.Do(req)
+	duration := time.Now().Sub(start)
+	if err != nil {
+		return nil, false, err
+	}
+	flag = judgment.TimeBase(elapsedMin, elapsedMax, duration)
+	return resp, flag, err
+}
+
+//表示判定を行うClientを提供する
+func (c *client) DisplayBaseJudgeDo(included string, req *http.Request) (resp *http.Response, flag bool, err error) {
+	resp, err = c.Do(req)
+	if err != nil {
+		return nil, false, err
+	}
+	body := tostring.Body(resp.Body)
+	flag = judgment.DisplayBase(body, included)
+	resp.Body = stringto.IoReadCloser(body)
+	return resp, flag, err
 }
