67ad278497495d5e835fc487dc1819f6648eb782
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-11-04 23:55:12 +0900

    init http client

--- client/client.go
+++ client/client.go
@@ -1,1 +1,30 @@
 package client
+
+import (
+	"log"
+	"net/http"
+)
+
+//New Clientを生成します
+func New(httpclient http.Client) Client {
+	return &client{httpclient}
+}
+
+//Client 各種特殊なクライアントを提供します
+type Client interface {
+}
+
+//Client 内部の情報を定義します
+type client struct {
+	httpclient http.Client
+}
+
+func (c *client) Do(req *http.Request) (resp *http.Response, err error) {
+	resp, err = c.httpclient.Do(req)
+	if err != nil {
+		log.Panicln(err)
+		return resp, err
+	}
+	defer resp.Body.Close()
+	return resp, err
+}
